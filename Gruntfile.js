d59758bcea3c37922c08077f4973fe626a701e4c
clement <maboiteaspam@wanadoo.fr>
2014-01-31 19:56:00 +0800

    Improved documentation support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,63 @@
 
 module.exports = function(grunt) {
 
-    var d = __dirname+"/vendors/phantomizer-strykejs";
-
-    var out_dir = d+"/demo/out/";
-    var meta_dir = d+"/demo/out/";
-
-
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-
-        ,"out_dir":out_dir
-        ,"meta_dir":meta_dir
-
-        //-
-        ,'phantomizer-strykejs-builder': {
-            options: {
-                "port":8080,
-                "ssl_port":8081,
-                "paths":[d+"/demo/in/"]
+        pkg: grunt.file.readJSON('package.json'),
+        docco: {
+            debug: {
+                src: [
+                    'taks/build.js'
+                ],
+                options: {
+                    layout:'linear',
+                    output: 'documentation/'
+                }
             }
-            ,test: {
-                options:{
-                    "in_request":"/index.html"
-                    ,"out": "<%= out_dir %>/index.html"
-                    ,"meta": "<%= meta_dir %>/index.html"
+        },
+        'gh-pages': {
+            options: {
+                base: '.',
+                add: true
+            },
+            src: ['documentation/**']
+        },
+        release: {
+            options: {
+                bump: true,
+                add: false,
+                commit: false,
+                npm: false,
+                npmtag: true,
+                tagName: '<%= version %>',
+                github: {
+                    repo: 'maboiteaspam/phantomizer-strykejs', //put your user/repo here
+                    usernameVar: 'GITHUB_USERNAME', //ENVIRONMENT VARIABLE that contains Github username
+                    passwordVar: 'GITHUB_PASSWORD' //ENVIRONMENT VARIABLE that contains Github password
                 }
             }
         }
     });
+    grunt.loadNpmTasks('grunt-docco');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-release');
 
-    grunt.loadNpmTasks('phantomizer-strykejs');
+    grunt.registerTask('cleanup-grunt-temp', [],function(){
+        var wrench = require('wrench');
+        wrench.rmdirSyncRecursive(__dirname + '/.grunt', !true);
+        wrench.rmdirSyncRecursive(__dirname + '/documentation', !true);
+    });
 
-    grunt.registerTask('default',
-        [
-            'phantomizer-strykejs-builder:test'
-        ]);
+    // to generate and publish the docco style documentation
+    // execute this
+    // grunt
+    grunt.registerTask('default', ['docco','gh-pages', 'cleanup-grunt-temp']);
+
+    // to release the project in a new version
+    // use one of those commands
+    // grunt --no-write -v release # test only
+    // grunt release:patch
+    // grunt release:minor
+    // grunt release:major
+    // grunt release:prerelease
+
 };
