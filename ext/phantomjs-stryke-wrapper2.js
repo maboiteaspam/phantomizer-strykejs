d995b48d973a887460e9d88a5f67c2145935b41f
clement <maboiteaspam@wanadoo.fr>
2014-01-31 20:09:28 +0800

    Updates

---
+++ ext/phantomjs-stryke-wrapper2.js
@@ -0,0 +1,113 @@
+
+
+var fs = require('fs');
+system = require("system");
+webpage = require("webpage");
+var in_urls_file = system.args[1];
+var out_urls_file = system.args[2];
+
+var data = fs.read(in_urls_file).toString();
+
+var target_urls = JSON.parse(data);
+
+iterate(0,20,end_iterate,0);
+
+
+function end_iterate(offset,limit,results,cnt_success){
+	console.log("done from:"+(offset-limit)+" to:"+offset+" success:"+cnt_success);
+	if( results == false ){
+		phantom.exit(0);
+	}else{
+		var index = offset-limit;
+		for( var n in results ){
+			var out_file = results[n].url_data.out_file;
+			var c = results[n].content;
+			fs.write(out_file, c, 'w');
+			index++;
+		}
+		iterate(offset,limit,end_iterate,cnt_success);
+	}
+}
+function iterate(offset,limit,cb,cnt_success){
+	var done = 0;
+	var results = [];
+	
+	if( target_urls.length == 0 ){
+		if( cb ) cb(offset,limit,false,cnt_success);
+	}
+	limit = limit<target_urls.length?limit:target_urls.length;
+	
+	for(var n=0;n<limit;n++){
+        (function(url_data){
+            retrieve_page(url_data.in_request,function(success,url,content){
+                done++;
+                offset++;
+                success?cnt_success++:success;
+                results.push({
+                    url_data:url_data,
+                    url:url,
+                    content:content,
+                    success:success
+                });
+                // var p = target_url.substring(target_url.lastIndexOf("/")+1);
+                // fs.write("results/page"+index+".html", content, 'w');
+                if( done == limit ){
+                    if( cb ) cb(offset,limit,results,cnt_success);
+                }
+            });
+        })(target_urls.shift());
+	}
+}
+
+function retrieve_page(target_url, cb){
+    var page = webpage.create();
+
+    page.onLoadStarted = function () {
+        console.log('Start loading...'+target_url);
+    };
+
+    page.onConsoleMessage = function(msg, lineNum, sourceId) {
+        console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+    };
+
+    page.onError = function(msg, trace) {
+        var msgStack = ['ERROR: ' + msg];
+        if (trace && trace.length) {
+            msgStack.push('TRACE:');
+            trace.forEach(function(t) {
+                msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+            });
+        }
+        console.error(msgStack.join('\n'));
+    };
+
+    page.onLoadFinished = function (status) {
+        console.log('finished...'+target_url);
+        var interval = null;
+        interval = window.setInterval(function () {
+            var a = page.evaluate(function (c) {
+                var a = document.getElementsByTagName("html")[0].getAttribute("class");
+                if (a) {
+                    if (a.indexOf("stryked") != -1 ){
+                        return document.getElementsByTagName("html")[0].outerHTML;
+                    }
+                }
+                return "";
+            });
+            if( a != "" ){
+                cb(true,target_url, a);
+                page.close();
+                clearInterval(interval);
+            }
+        }, 10);
+    };
+
+
+    page.open(target_url, function (b) {
+        if( b !== "success"){
+            console.log("Unable to access network "+target_url);
+        }else{
+            page.evaluate(function () {});
+        }
+    });
+}
---
+++ tasks/build.js
@@ -0,0 +1,460 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    var childProcess    = require('child_process');
+    var phantomjs       = require('phantomjs');
+    var ph_libutil      = require("phantomizer-libutil");
+    var dirlisting      = require("phantomizer-html-dirlisting");
+    var fs              = require("fs");
+    var connect         = require('connect');
+    var http            = require('http');
+    var path            = require('path');
+
+    var meta_factory = ph_libutil.meta;
+
+    var http_utils          = ph_libutil.http_utils;
+    var router_factory      = ph_libutil.router;
+    var file_utils          = ph_libutil.file_utils;
+    var optimizer_factory   = ph_libutil.optimizer;
+    var phantomizer_helper  = ph_libutil.phantomizer_helper;
+
+    grunt.registerMultiTask("phantomizer-strykejs-builder", "Builds html dependencies of a stryke file", function () {
+
+        var wd = process.cwd();
+
+        var config = grunt.config();
+        var options = this.options({
+            in_request:null,
+            port:null,
+            ssl_port:null,
+            out_file:null,
+            paths:null,
+            meta_file:null,
+            meta_dir:null,
+            scripts:null,
+            css:null,
+            log:false
+        });
+        var in_request  = options.in_request;
+        var port        = options.port;
+        var ssl_port    = options.ssl_port;
+        var out_file    = options.out;
+        var paths       = options.paths;
+        var meta_file   = options.meta;
+        var meta_dir    = options.meta_dir;
+
+        var current_grunt_task = this.nameArgs;
+        var current_grunt_opt = this.options();
+        var user_config = grunt.config();
+
+        var meta_manager = new meta_factory( wd, meta_dir );
+        var optimizer = new optimizer_factory(meta_manager, config);
+        var router = new router_factory(user_config.routing);
+
+        var done = this.async();
+        router.load(function(){
+            // check if a cache entry exists, if it is fresh, just serve it
+            if( meta_manager.is_fresh(meta_file) == false ){
+
+                var req_logs = {}
+                var app = get_webserver(router, optimizer, options, req_logs);
+                var wserver = http.createServer(app).listen(port);
+
+                var finish = function(res){
+                    // return;
+                    if( res == true ){
+                        grunt.log.ok()
+                        done(true);
+                    }else{
+                        grunt.log.error(res)
+                        done(false);
+                    }
+                }
+
+                var deps = []
+                var route = router.match(in_request);
+                var file = file_utils.find_file(paths,route.template);
+                deps.push(file);
+
+                var target_url = "http://localhost:"+port+in_request;
+                var wrapper = __dirname+'/../ext/phantomjs-stryke-wrapper.js';
+                execute_phantomjs([wrapper, target_url, out_file],function(err, stdout, stderr){
+                    wserver.close();
+
+
+                    var retour = grunt.file.read(out_file);
+// remove stryke configuration used to prevent full execution of the page
+                    retour = remove_stryke( retour );
+// remove requirejs scripts, they are put in the head on runtime
+                    retour = remove_rjs_trace( retour );
+// get traced url call from runtime, remove it from output
+                    var trace = extract_stryke_trace( retour )
+                    retour = remove_stryke_trace( retour )
+                    if( trace.length > 0 ){
+                        trace.unshift(in_request)
+                        for(var n in trace){
+                            deps.push(req_logs[trace[n]])
+                        }
+                    }
+                    // add grunt file to dependencies so that file are rebuild when this file changes
+                    deps.push(__filename)
+                    if ( grunt.file.exists(process.cwd()+"/Gruntfile.js")) {
+                        deps.push(process.cwd()+"/Gruntfile.js")
+                    }
+                    if ( grunt.file.exists(user_config.project_dir+"/../config.json")) {
+                        deps.push( user_config.project_dir+"/../config.json")
+                    }
+                    // create a cache entry, so that later we can regen or check freshness
+                    var entry = meta_manager.create(deps)
+                    entry.require_task(current_grunt_task, current_grunt_opt)
+                    entry.save(meta_file, function(err){
+                        if (err) finish(err)
+                        else{
+                            grunt.file.write(out_file, retour)
+                            finish(true)
+                        }
+                    })
+                }).stdout.on('data', function (data) {
+                    console.log(data.trim())
+                });
+            }else{
+                grunt.log.ok("the build is fresh")
+                done(true);
+            }
+        });
+    });
+
+
+
+    grunt.registerMultiTask("phantomizer-strykejs-builder2", "Builds html dependencies of a stryke file", function () {
+
+        var wd = process.cwd();
+
+        var config = grunt.config();
+        var options = this.options({
+            run_dir:'',
+            meta_dir:'',
+            port:'',
+            ssl_port:'',
+            urls_file:'',
+            paths:[],
+            scripts:null,
+            css:null,
+            log:false
+        });
+        var run_dir     = options.run_dir;
+        var meta_dir    = options.meta_dir;
+        var port        = options.port;
+        var ssl_port    = options.ssl_port;
+        var urls_file   = options.urls_file;
+
+        var paths = options.paths;
+
+        var current_grunt_target = this.target;
+        var user_config = grunt.config();
+
+        var meta_manager = new meta_factory( wd, meta_dir );
+        var optimizer = new optimizer_factory(meta_manager, config);
+        var router = new router_factory(config.routing);
+
+        var req_logs = {}
+        var app = get_webserver(router, optimizer, options, req_logs);
+        var wserver = http.createServer(app).listen(port);
+        var wsserver = http.createServer(app).listen(ssl_port);
+
+        var done = this.async();
+        router.load(function(){
+
+            var finish = function(res){
+                if( res == true ){
+                    grunt.log.ok()
+                    done(true);
+                }else{
+                    grunt.log.error(res)
+                    done(false);
+                }
+            }
+
+            // fetch urls to build
+            var raw_urls = grunt.file.readJSON(urls_file)
+            grunt.log.ok("URL Count "+raw_urls.length);
+
+            var strykejs_urls_file = run_dir+"/tmp/strykejs-urls.json";
+            grunt.file.mkdir( path.dirname(strykejs_urls_file) )
+            for( var n in raw_urls ){
+                raw_urls[n].in_request = "http://localhost:"+port+raw_urls[n].in_request+"";
+            }
+            grunt.file.write(strykejs_urls_file, JSON.stringify(raw_urls));
+
+            var wrapper = __dirname+'/../ext/phantomjs-stryke-wrapper2.js';
+            execute_phantomjs([wrapper, strykejs_urls_file], function(err, stdout, stderr){
+                wserver.close();
+                wsserver.close();
+
+                grunt.file.delete(strykejs_urls_file);
+                //-
+                for( var n in raw_urls ){
+                    var in_request = raw_urls[n].in_request;
+                    var meta_file = raw_urls[n].meta_file;
+                    var out_file = raw_urls[n].out_file;
+                    var deps = [];
+
+                    var retour = grunt.file.read(out_file);
+                    // remove stryke configuration used to prevent full execution of the page
+                    retour = remove_stryke( retour );
+                    // remove requirejs scripts, they are put in the head on runtime
+                    retour = remove_rjs_trace( retour );
+                    // get traced url call from runtime, remove it from output
+                    var trace = extract_stryke_trace( retour )
+                    retour = remove_stryke_trace( retour )
+                    if( trace.length > 0 ){
+                        trace.unshift(in_request);
+                        for(var n in trace){
+                            deps.push(req_logs[trace[n]]);
+                        }
+                    }
+
+                    // add grunt file to dependencies so that file are rebuild when this file changes
+                    deps.push(__filename)
+                    if ( grunt.file.exists(process.cwd()+"/Gruntfile.js")) {
+                        deps.push(process.cwd()+"/Gruntfile.js")
+                    }
+                    if ( grunt.file.exists(user_config.project_dir+"/../config.json")) {
+                        deps.push( user_config.project_dir+"/../config.json")
+                    }
+                    var route = router.match(in_request);
+                    if( route != false ){
+                        var file = file_utils.find_file(paths,route.template);
+                        deps.push(file);
+                    }
+                    // create a cache entry, so that later we can regen or check freshness
+                    var entry = meta_manager.create(deps);
+
+                    // save phantomizer-html-builder2 task options
+                    var opt = grunt.config.get("phantomizer-html-builder2");
+                    if(!opt[current_grunt_target]) opt[current_grunt_target] = {};
+                    if(!opt[current_grunt_target].options) opt[current_grunt_target].options = {};
+                    opt[current_grunt_target].options.url = in_request;
+                    entry.require_task("phantomizer-html-builder2:"+current_grunt_target, opt[current_grunt_target]);
+
+                    entry.save(meta_file, function(err){
+                        grunt.file.write(out_file, retour)
+                    })
+                }
+                finish(true)
+            }).stdout.on('data', function (data) {
+                console.log(data.trim())
+            });
+
+        });
+    });
+
+
+    function get_webserver(router, optimizer, options, req_logs){
+        var paths = options.paths;
+        var requirejs_src = options.scripts.requirejs.src;
+        var requirejs_baseUrl = options.scripts.requirejs.baseUrl;
+        var requirejs_paths = options.scripts.requirejs.paths;
+
+        var app = connect();
+        app.use(connect.query())
+        app.use(connect.urlencoded())
+        if( options.log == true ){
+            app.use(connect.logger('dev'))
+        }
+        app.use(function(req, res, next){
+
+            var request_path = get_request_path( req.originalUrl )
+
+            if(request_path.match(/^\/stryke_b64/) ){
+                request_path = request_path.replace( /^\/stryke_b64/, "" );
+
+                var file = file_utils.find_file(paths, request_path);
+
+                if( file ){
+                    var buf = fs.readFileSync(file).toString('base64');
+                    var headers = {
+                        'Content-Type': http_utils.header_content_type(file)
+                    };
+                    res.writeHead(200, headers)
+                    res.end("data:"+headers['Content-Type']+";base64,"+ buf )
+                }else{
+                    next()
+                }
+            }else{
+                next()
+            }
+        })
+// routed request, html only
+        app.use(function(req, res, next){
+            var request_path = get_request_path( req.originalUrl )
+            var headers = {
+                'Content-Type': http_utils.header_content_type(request_path)
+            };
+            var route = router.match(request_path);
+            if( route != false && headers["Content-Type"].indexOf("text/") > -1 ){
+                var file = file_utils.find_file(paths,route.template);
+                if(! file ){
+                    grunt.log.error("Cannot find the template "+route.template+" for url route "+request_path);
+                    next()
+                }else{
+                    req_logs[request_path] = file;
+                    var buf = fs.readFileSync(file);
+                    if( headers["Content-Type"].indexOf("text/") > -1 ){
+                        buf = buf.toString();
+                    }
+                    var base_url = request_path.substring(0,request_path.lastIndexOf("/")) || "/";
+                    if( options.scripts ){
+                        create_combined_assets(optimizer, options.scripts, paths);
+                        buf = phantomizer_helper.apply_scripts(options.scripts, base_url, buf);
+                    }
+                    if( options.css ){
+                        create_combined_assets(optimizer, options.css, paths);
+                        buf = phantomizer_helper.apply_styles(options.css, base_url, buf);
+                    }
+                    buf = phantomizer_helper.inject_requirejs(requirejs_baseUrl, requirejs_src, requirejs_paths, buf, null);
+                    buf = add_stryke(buf);
+                    res.writeHead(200, headers)
+                    res.end(buf)
+                }
+            }else{
+                next()
+            }
+        })
+// various asset including text / binary
+        app.use(function(req, res, next){
+            var request_path = get_request_path( req.originalUrl )
+            var headers = {
+                'Content-Type': http_utils.header_content_type(request_path)
+            };
+            var file = file_utils.find_file(paths,request_path);
+            if( file ){
+                req_logs[request_path] = file;
+                var buf = fs.readFileSync(file);
+                if( headers["Content-Type"].indexOf("text/") > -1 ){
+                    buf = buf.toString();
+                }
+                res.writeHead(200, headers)
+                res.end(buf)
+            }else{
+                next()
+            }
+        })
+// directory listing
+        app.use(function(req, res, next){
+            var request_path = get_request_path( req.originalUrl )
+            var file = file_utils.find_dir(paths,request_path);
+            if( file != null ){
+                var items = http_utils.merged_dirs(paths, request_path);
+                dirlisting.generate_directory_listing(items, function(err, html){
+                    var headers = {
+                        'Content-Type': 'text/html'
+                    };
+                    res.writeHead(200, headers);
+                    res.end(html);
+                });
+            }else{
+                next()
+            }
+        })
+        app.use(function(req, res){
+            var headers = {
+                'Content-Type': 'text/html'
+            };
+            res.writeHead(404, headers)
+            res.end("not found")
+        })
+
+        return app;
+    }
+    function get_request_path( request_path ){
+        if( request_path.indexOf("?")>-1){
+            request_path = request_path.substring(0,request_path.indexOf("?"))
+        }
+        return request_path
+    }
+    function add_stryke( in_str ){
+        var stryke = ""
+        stryke = stryke+"<script>"
+        stryke = stryke+    "var phantomatic = true;"
+        stryke = stryke+"</script>"
+        in_str = in_str.replace("</head>", stryke+"</head>")
+        return in_str
+    }
+    function create_combined_assets(optimizer, assets_combination, source_paths){
+        var target_merge="";
+        if( assets_combination.append ){
+            for( target_merge in assets_combination.append ){
+                if( target_merge.length > 1 ){
+                    var asset_deps = assets_combination.append[target_merge];
+                    optimizer.merge_files(target_merge, asset_deps, source_paths);
+                    grunt.verbose.ok("merged "+target_merge+"")
+                }
+            }
+        }
+        if( assets_combination.prepend ){
+            for( target_merge in assets_combination.prepend ){
+                if( target_merge.length > 1 ){
+                    var asset_deps = assets_combination.prepend[target_merge];
+                    optimizer.merge_files(target_merge, asset_deps, source_paths);
+                    grunt.verbose.ok("merged "+target_merge+"")
+                }
+            }
+        }
+    }
+    function execute_phantomjs(args, cb){
+        var childArgs = [ '--load-images=false' ];
+        for(var n in args )childArgs.push(args[n])
+
+        grunt.verbose.writeln(phantomjs.path+" "+childArgs.join(" "));
+
+        grunt.log.ok("Starting PhantomJS... ");
+        return childProcess.execFile(phantomjs.path, childArgs, function(err, stdout, stderr) {
+            grunt.log.ok("... Done PhantomJS");
+            if( stderr != "" ){
+                console.log(stderr)
+                grunt.log.error("... PhantomJS failed");
+            }
+            cb(err, stdout, stderr);
+        });
+    }
+
+
+
+
+    function remove_stryke( in_str ){
+        var stryke = ""
+        stryke = stryke+"<script>"
+        stryke = stryke+    "var phantomatic = true;"
+        stryke = stryke+"</script>"
+        in_str = in_str.replace(stryke+"", "")
+        return in_str
+    }
+    function remove_rjs_trace( in_str ){
+        var ptn = /<script type="text\/javascript" charset="utf-8" async="" data-requirecontext="_"[^>]*><\/script>/g
+        in_str = in_str.replace(ptn,"");
+        return in_str
+    }
+    function extract_stryke_trace( in_str ){
+        var ptn = /<div id="stryke_trace">([^<]*?)<\/div>/gi
+        var retour = []
+        var trace = in_str.match(ptn);
+        if( trace != null && trace.length > 0 ){
+            trace=trace[0]
+            trace=trace.substring( ('<div id="stryke_trace">').length )
+            trace=trace.substring( 0, trace.length-('</div>').length )
+            trace=trace.split(/\r\n|\r|\n/);
+            retour = trace;
+        }
+        return retour
+    }
+    function remove_stryke_trace( in_str ){
+        var ptn = /<div id="stryke_trace">([^<]*?)<\/div>/gi
+        var trace=in_str.match(ptn);
+        if( trace != null && trace.length > 0 ){
+            in_str = in_str.replace(trace[0],"")
+        }
+        return in_str
+    }
+};
